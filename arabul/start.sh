f02ef622a9c477b28ca77364bf969c708c02b717
Enes Talha Zor <enestalhazorr@gmail.com>
2025-11-24 20:43:05 +0300

    add static server for productphotos

--- arabul/start.sh
+++ arabul/start.sh
@@ -2,5 +2,9 @@
 
 service postgresql start
 service postgresql status
+
+# Start static server here
+serve -s /productphotos -l 0.0.0.0:8090 &
+
 echo "starting application"
 java -jar /home/app.jar
